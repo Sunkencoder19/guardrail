361cddced7232588929eaefd6ec4b16cf14ebf97
Sunkencoder19 <cypher1906@gmail.com>
2026-08-14 14:41:53 +0530

    fix: replace native bcrypt with bcryptjs

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import ApiError from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 
